4330ee9cd33d68a177bd3b4773efcd83badb7fab
COJOS <c0j0s@outlook.com>
2021-11-06 01:57:14 +0800

    Update lab8

--- lab8/Logger.java
+++ lab8/Logger.java
@@ -11,18 +11,18 @@ class Logger<T> {
 
     private Optional<T> cache;
     private final Supplier<T> supplier;
-    private final List<Supplier<String>> log;
+    private final Supplier<List<Supplier<String>>> history;
     private boolean evaluated;
 
     private Logger(Supplier<T> supplier) {
-        this(supplier, new ArrayList<Supplier<String>>());
+        this(supplier, () -> new ArrayList<Supplier<String>>());
     }
 
-    private Logger(Supplier<T> supplier, List<Supplier<String>> log) {
-        this.log = log;
+    private Logger(Supplier<T> supplier, Supplier<List<Supplier<String>>> history) {
         this.supplier = supplier;     
         this.cache = Optional.empty();
         evaluated = false;
+        this.history = history;
     }
 
     public static <T> Logger<T> of(T v) throws IllegalArgumentException {
@@ -46,17 +46,21 @@ class Logger<T> {
     }
 
     <U> Logger<U> map(Function<? super T, ? extends U> mapper) {
-        List<Supplier<String>> newlog = new ArrayList<Supplier<String>>();
-        newlog.addAll(log);
-        newlog.add(() -> String.format("%s -> %s", get(), mapper.apply(get())));
-        return new Logger<U>(() -> mapper.apply(get()), newlog);
+        return new Logger<U>(() -> mapper.apply(get()), () -> {
+            List<Supplier<String>> newlog = new ArrayList<Supplier<String>>();
+            newlog.addAll(history.get());
+            newlog.add(() -> get().toString());
+            return newlog;
+        });
     }
 
     <U> Logger<U> flatMap(Function<? super T, ? extends Logger<? extends U>> mapper) {
-        List<Supplier<String>> newlog = new ArrayList<Supplier<String>>();
-        newlog.addAll(log);
-        newlog.add(() -> mapper.apply(get()).printLog());
-        return new Logger<U>(() -> mapper.apply(get()).get(), newlog);    
+        return new Logger<U>(() -> mapper.apply(get()).get(), () -> {
+            List<Supplier<String>> newlog = new ArrayList<Supplier<String>>();
+            newlog.addAll(history.get());
+            newlog.addAll(mapper.apply(get()).getLog());
+            return newlog;
+        });    
     }
 
     Logger<T> test(Predicate<? super T> pred, Logger<T> trueLogger, Logger<T> falseLogger) {
@@ -73,22 +77,24 @@ class Logger<T> {
         return false;
     }
 
+    List<Supplier<String>> getLog() {
+        return history.get();
+    }
+
     String printLog() {
-        return log.stream().map(x -> x.get()).collect(Collectors.joining("\n"));
+        List<String> log = getLog().stream().map(x -> x.get()).collect(Collectors.toList());
+        String op = "";
+        for (int i = 0; i < log.size() - 1; i++) {
+            op += log.get(i) + " -> " + log.get(i + 1) + "\n";      
+        }
+        if (log.size() > 0) {
+            op += log.get(log.size() - 1) + " -> " + get();
+        }
+        return op;
     }
 
     @Override
     public String toString() {
-        // to prevent evaluation of last element again
-        String logs = printLog();
-        String op = "";
-        if (log.size() > 0) {
-            op = logs.substring(logs.lastIndexOf(">") + 1).trim();
-        } 
-        // ensure final op has a value
-        if (op.isEmpty()) {
-            op = get().toString();
-        }
-        return (String.format("Logger[%s]", op) + "\n" + logs).trim();
+        return (String.format("Logger[%s]", get()) + "\n" + printLog()).trim();
     }
 }
